8e0ef4d7fa4dec31df2b4481c1205360f483e875
Nico Prananta <nico@hyperjump.tech>
2019-12-26 19:43:35 +0800

    flatten the published directory

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"]
+  presets: ["module:metro-react-native-babel-preset"],
+  env: {
+    production: {
+      ignore: [
+        "**/*.test.js"
+      ],
+      sourceMaps: true
+    }
+  }
 };
