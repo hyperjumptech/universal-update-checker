09b8c5bad00617a4c2acb3bca9cde207faad06ec
Nico Prananta <nicnocquee@users.noreply.github.com>
2019-12-27 11:51:05 +0800

    Add eslint and prettier (#8)
    * add eslint to lint
    
    * add prettier check
    
    * bump version
    
    * move babel-eslint to dev dep
    
    Co-authored-by: nico-hyperjump <58848001+nico-hyperjump@users.noreply.github.com>

--- src/default-local-remote.js
+++ src/default-local-remote.js
@@ -1,22 +1,22 @@
-import DeviceInfo from 'react-native-device-info';
-import firebase from 'react-native-firebase';
+import DeviceInfo from "react-native-device-info";
+import firebase from "react-native-firebase";
+import { Platform } from "react-native";
 
 /**
  * get the current version from the app's build number on android and the version string from iOS
  */
 export const getLocalVersion = () => {
   if (DeviceInfo) {
-    if (Platform.OS === 'ios') {
+    if (Platform.OS === "ios") {
       const readableVersion = DeviceInfo.getVersion();
       return readableVersion;
-    } else if (Platform.OS === 'android') {
+    } else if (Platform.OS === "android") {
       const readableVersion = DeviceInfo.getBuildNumber();
       return readableVersion;
     }
   }
   return null;
 };
-
 
 /**
  * get the latest and required version from firebase
@@ -28,40 +28,41 @@ export const getVersionsFromFirebase = () => {
     .then(() => {
       return firebase.config().activateFetched();
     })
-    .then((activated) => {
+    .then(() => {
       return firebase.config().getValues([
         // these properties need to be added in firebase remote config console
-        'required_version_ios',
-        'latest_version_ios',
-        'required_version_android',
-        'latest_version_android',
+        "required_version_ios",
+        "latest_version_ios",
+        "required_version_android",
+        "latest_version_android"
       ]);
     })
-    .then((objects) => {
+    .then(objects => {
       let data = {};
-      Object.keys(objects).forEach((key) => {
+      Object.keys(objects).forEach(key => {
         data[key] = objects[key].val();
       });
 
       var latest;
       var required;
-      if (Platform.OS === 'ios') {
+      if (Platform.OS === "ios") {
         latest = data.latest_version_ios;
         required = data.required_version_ios;
-      } else if (Platform.OS === 'android') {
+      } else if (Platform.OS === "android") {
         latest = data.latest_version_android;
         required = data.required_version_android;
       }
 
       return {
         latest,
-        required,
+        required
       };
     })
-    .catch((error) => {
+    .catch(error => {
       return {
         latest: null,
         required: null,
+        error
       };
     });
 };
@@ -81,48 +82,48 @@ export const getVersionsFromFirebase = () => {
 export const getUpdateStatus = (
   currentVersion,
   latestVersion,
-  requiredVersion,
+  requiredVersion
 ) => {
   let digitAndDotsOnlyRegex = /^\d+(\.\d+)*$/;
   if (!digitAndDotsOnlyRegex.test(DeviceInfo.getVersion())) {
     // If the version has non-numerical and dot characters, return "none".
     // During development, the version could have alphabet characters, e.g., 6.3-dev.
-    return 'none';
+    return "none";
   }
 
-  if (Platform.OS === 'ios') {
+  if (Platform.OS === "ios") {
     // function to convert version string (x.x.x) to a number value.
-    const versionToValue = (version) => {
-      var parts = version.split('.').reverse();
+    const versionToValue = version => {
+      var parts = version.split(".").reverse();
       return parts.reduce(
         (prev, curr, index) => prev + Math.pow(10, index) * curr,
-        0,
+        0
       );
     };
     if (!latestVersion || !requiredVersion) {
-      return 'none';
+      return "none";
     }
     const requiredVersionValue = versionToValue(requiredVersion);
     const latestVersionValue = versionToValue(latestVersion);
     const localVersionValue = versionToValue(currentVersion);
     if (localVersionValue < requiredVersionValue) {
-      return 'required';
+      return "required";
     } else if (
       localVersionValue >= requiredVersionValue &&
       localVersionValue < latestVersionValue
     ) {
-      return 'latest';
+      return "latest";
     }
-    return 'none';
-  } else if (Platform.OS === 'android') {
+    return "none";
+  } else if (Platform.OS === "android") {
     if (currentVersion < requiredVersion) {
-      return 'required';
+      return "required";
     } else if (
       currentVersion >= requiredVersion &&
       currentVersion < latestVersion
     ) {
-      return 'latest';
+      return "latest";
     }
-    return 'none';
+    return "none";
   }
 };
